d0a4bcf520180aee050470a41f0865435e3f047e
Void <trigger_austin00@proton.me>
2024-05-04 12:02:23 +0300

    done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,55 @@
 #!/usr/bin/python3
+"""
+defines a class rectangle
+"""
+
+
+class Rectangle:
+    """ 
+        Class of rectangle
+
+        Attributes:
+            Width: private instance attribute width
+            Height: private instance attribute height
+    """
+
+    def __init__(self, width=0, height=0):
+        """set width and height to 0"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self._height = height
+        self._width = width
+
+    @property
+    def width(self):
+        """getter for width"""
+        return self._width
+
+    @property
+    def height(self):
+        """getter for height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """setter for width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
