51834cc5ee7d64db8c11d4457eeebfb8631a48fa
penality <trigger_austin00@proton.me>
2024-07-17 21:49:30 +0300

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a method to look up methods and attr of an object
+"""
+
+def lookup(obj):
+    """
+    Return list of attributes or methods
+    args:
+        obj - object containing said attributes
+    Returns:
+        list of attr or methods
+    """
+
+    return dir(obj)
