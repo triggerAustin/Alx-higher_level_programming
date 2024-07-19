497f3047921ffef79dbbfd06bd3e46e8e143c837
penality <trigger_austin00@proton.me>
2024-07-19 11:24:58 +0300

    file 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        a class BaseGeometry
+    """
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
