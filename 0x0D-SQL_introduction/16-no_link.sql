3b89ba11f5af8d6419d351488dc4a52cbaf4f21f
penality <trigger_austin00@proton.me>
2024-07-28 12:05:55 +0300

    sql scripts for db manipulation

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table
-SHOW score, name FROM  second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM  second_table WHERE name IS NOT NULL ORDER BY score DESC;
