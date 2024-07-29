80d295eb1bff06d8a125e53d7bb25ec09630e446
penality <trigger_austin00@proton.me>
2024-07-29 22:01:33 +0300

    all tasks

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-    Script that takes in an argument and displays all values in the states table
+    Script that takes in an argument and 
+    displays all values in the states table
     of hbtn_0e_0_usa where name matches the argument.
 """
 import MySQLdb
