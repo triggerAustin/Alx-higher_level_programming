fc77eb9227dae73a49cf806d9735a9043cd00054
penality <trigger_austin00@proton.me>
2024-07-29 21:41:18 +0300

    js tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 let arr = [args[1]...];
 int i, min = 0;
-for(i = 0; i < arr.length; i++){
+for (i = 0; i < arr.length; i++){
 	if(arr[i] < min){
 		min = arr[i];
 	}
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-
 const args = process.argv;
-
 let x = args[1];
 let y = args[2];
-
 export function add(a, b){
 	return(a + b);
 }
-
 add(x, y);
