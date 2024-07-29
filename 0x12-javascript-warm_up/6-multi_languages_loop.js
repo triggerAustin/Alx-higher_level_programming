06417d6d69259d110dfc4fc2dc1100f7cb32efb3
penality <trigger_austin00@proton.me>
2024-07-29 21:39:34 +0300

    js tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
-while(i < arr.length){
+while (i < arr.length){
 	console.log(arr[i]);
 	i++;
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,12 @@
 const args = process.argv;
 let i = 0;
 let j;
-if(!Number.isInteger(parseInt(args[2]))){
+if (!Number.isInteger(parseInt(args[2]))){
 	console.log("Missing number of occurrences");
 	return;
 }
 j = args[2];
-while(i < j){
+while (i < j){
 	console.log("C is fun");
 	i++;
 }
