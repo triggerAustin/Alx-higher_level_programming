650cc2d4e3546ef96cb469bf03152397ccdbc861
penality <trigger_austin00@proton.me>
2024-07-29 21:35:04 +0300

    js tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-
 const args = process.argv;
 let i = 0;
 let j;
-
 if(!Number.isInteger(parseInt(args[2]))){
 	console.log("Missing number of occurrences");
 	return;
 }
-
 j = args[2];
 while(i < j){
 	console.log("C is fun");
