c6bd3e1afa5d4e435b4548e985871671200ecc15
penality <trigger_austin00@proton.me>
2024-07-30 14:10:39 +0300

    task 6: documentation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,12 +10,13 @@ def find_peak(list_of_integers):
         time complexity O(log n)
         Args:
             list_pf_integers: array
+        Return: returns the peak value in arr
     """
     size = len(list_of_integers)
 
     if !size:
         return None
-    
+ 
     arr = list_of_integers
     mid_idx = size // 2
 
@@ -24,7 +25,7 @@ def find_peak(list_of_integers):
         right = mid_idx + 1
 
         if (arr[mid_idx] > arr[left] and arr[mid_idx] > arr[right]
-            or mid_idx == size - 1 or mid_idx == 0):
+                or mid_idx == size - 1 or mid_idx == 0):
             return arr[mid_idx]
         if (arr[mid_idx] < arr[left]):
             mid_idx = left
