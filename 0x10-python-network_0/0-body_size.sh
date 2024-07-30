04528f763ff7c24db2212b33732908cde0aa3c8e
penality <trigger_austin00@proton.me>
2024-07-30 13:34:10 +0300

    task 0: fixed syntax

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # check if url is passed as arg
-if [ -z "$1" ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-URL=$1
-
-# send request using curl to url
-
-curl -s -o /dev/null -w '%{size_download}\n' "$URL"
+curl -s -o /dev/null -w '%{size_download}\n' "$1"
