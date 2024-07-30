27ee19b99979a6b36bbf2855bd74d7b06cdf188a
penality <trigger_austin00@proton.me>
2024-07-30 17:05:44 +0300

    task 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/*
+ script that writes a string to a file.
+ first argument is the file path
+ second argument is the string to write
+ content of the file must be written in utf-8
+ print the error object if error occurs
+ */
+
+const args = process.argv.slice(2)
+const fs = require('fs')
+
+fs.writeFile(args[0], args[1], 'utf8', (error) => {
+	if (error){
+		console.log(error);
+	}
+});
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ script that display the status code of a GET request
+ first argument is the URL to request (GET)
+ status code must be printed like this: code: <status code>
+ must use the module request
+ */
+const args = process.argv.slice(2);
+const 
