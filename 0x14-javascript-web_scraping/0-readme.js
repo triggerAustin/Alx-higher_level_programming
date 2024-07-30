c740e523071c2df7f325cc8c519cb7a3157f53f2
penality <trigger_austin00@proton.me>
2024-07-30 16:51:31 +0300

    task 0:refactor

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const path = require('path');
-const filePath = path.resolve(process.argv[2]);
+const filePath = process.argv.slice(2)[0]
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.log(err);
+fs.readFile(filePath, 'utf8', (error, data) => {
+	if (error) {
+		console.log(error);
 	}
 	else{
 		console.log(data);
