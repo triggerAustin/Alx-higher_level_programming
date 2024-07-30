3d1ab4a4c5a547a11f4cd99bfdc6cdf9a60bb5d7
penality <trigger_austin00@proton.me>
2024-07-30 23:03:25 +0300

    task 2: fixed spelling err

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,7 +8,7 @@
 const args = process.argv.slice(2);
 const request = require('request') ;
 
-request(args[0], functoin(error, response, body){
+request(args[0], function(error, response, body){
 	if (response && !error){
 		console.log("code:", response.statusCode);
 	}
