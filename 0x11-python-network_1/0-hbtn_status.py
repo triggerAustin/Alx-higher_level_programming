9c64dbc3b189f8ae000a7fb9f090cf5d4709df99
penality <trigger_austin00@proton.me>
2024-07-30 15:55:21 +0300

    file creation, task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,14 +4,3 @@ Fetches https://alx-intranet.hbtn.io/status
 displays body response
 """
 
-import urllib.request
-
-url = 'https://alx-intranet.hbtn.io/status'
-
-with urllib.request.urlopen(url) as response:
-    body = respones.read().decode('utf-8')
-
-print("Body response:")
-print(f"\t - type: {type(body)}")
-print(f"\t - body: {body}")
-print(f"\t - utf8 content: {body.encode('utf-8')}")
