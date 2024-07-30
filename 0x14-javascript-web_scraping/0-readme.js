a2eb5322e077ce5537ad563946aeaf3197c6b50f
penality <trigger_austin00@proton.me>
2024-07-30 16:55:00 +0300

    task 0: documented

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,7 @@
 const fs = require('fs');
 const filePath = process.argv.slice(2)[0]
 
-fs.readFile(filePath, 'utf8', (error, data) => {
+fs.readFile (filePath, 'utf8', (error, data) => {
 	if (error) {
 		console.log(error);
 	}
