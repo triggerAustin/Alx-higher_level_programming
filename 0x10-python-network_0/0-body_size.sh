e70a1631b9d6031c1a84519404eec3fd254b3dc8
penality <trigger_austin00@proton.me>
2024-07-30 13:30:26 +0300

    task 0: send request to url"

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# check if url is passed as arg
+if [ -z "$1" ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+URL=$1
+
+# send request using curl to url
+
+curl -s -o /dev/null -w '%{size_download}\n' "$URL"
