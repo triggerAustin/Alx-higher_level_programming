ae490fe7ee8155c45f53c91b371f29a89acb149d
penality <trigger_austin00@proton.me>
2024-08-10 11:43:09 +0300

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     values = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
-    request = urllib.request.Request(sys.argv[1], data=data)
+    request = urllib.request.Request(sys.argv[1], data)
     with urllib.request.urlopen(request) as response:
         the_page = response.read().decode('utf-8')
         print(the_page)
