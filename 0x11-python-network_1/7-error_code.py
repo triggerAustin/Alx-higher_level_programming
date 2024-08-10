c867c2ee6da8d7ea82f38f42763571a0021986af
penality <trigger_austin00@proton.me>
2024-08-10 16:51:41 +0300

    task 7:

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL,
+sends a request to the URL and displays the body of the response.
+"""
+
+
+import requests
+import sys
+
+def send_req(url):
+    """
+        sends request to url passed as a param and
+        return an error
+
+        Args:
+            url: url to send req to
+    """
+    req = requests.get(url)
+    if req.status_code > 400:
+        print("Error code:", r.status_code)
+    else:
+        print(req.text)
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    send_req(url)
